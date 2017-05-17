d295bf587a9d0ea8004aa73dbd5f5754987cbbc8
Benjamin Jacobs <benjaminpjacobs@gmail.com>
2017-05-16 19:37:08 -0600

    sad path for adding song

--- spec/features/user_creates_song_spec.rb
+++ spec/features/user_creates_song_spec.rb
@@ -14,4 +14,20 @@ RSpec.feature "User submits a new song" do
     expect(page).to have_content song_title
     expect(page).to have_link artist.name, href: artist_path(artist)
   end
+
+  context "they forgot to enter the title'" do
+    scenario "they see an error message" do
+
+      artist = create(:artist)
+
+      visit artist_path(artist)
+      click_on "New song"
+      click_on "Create Song"
+
+      expect(page).to have_content "Title can't be blank"
+      expect(current_path).to eq("/artists/#{artist.id}/songs")
+    end
+    
+  end
+
 end
